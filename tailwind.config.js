2f3c87cab5c3d77aede15af1dc4173fe404ed7e2
kanishk tiwari <kanishk.swe@gmail.com>
2024-04-02 09:53:42 +0530

    initial commit -project initialized

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#ee0181'
+      },
+      fontFamily: {
+        'primary': ['Inter', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
+
